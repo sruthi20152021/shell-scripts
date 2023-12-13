61c87a78f4453980c342bbc8f1d6f61080197ae6
Sruthi Kallu <sruthi20152021@gmail.com>
2023-12-13 01:27:23 -0600

    added 2nd script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/Bbash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hello $Person2, Good Morning"
+echo "$Person2: Hello $Person1, Good Morning"
+echo "$Person1: How are you $Person2"
+echo "$Person2: How are you $Person1"
