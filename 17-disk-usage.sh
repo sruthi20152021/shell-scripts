5d59e2cbf715372e769dab2fa9032a52af1858e1
Sruthi Kallu <sruthi20152021@gmail.com>
2023-12-31 00:03:03 -0600

    shell-scripts

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -8,8 +8,10 @@ while IFS= read line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partion=$(echo $line  | awk '{print $1F}')
-    if [ $usage -gt $DSIK_THRESHOLD ]
+    if [ $usage -ge $DSIK_THRESHOLD ]
     then
         message+="High Disk Usage 0n $partition: $usage"
     fi
 done <<< $DISK_USAGE
+
+echo "Message: $message"
