68c5e361e0878cfd4db6dc08748d1aa6999ce21e
Sruthi Kallu <sruthi20152021@gmail.com>
2023-12-30 23:52:02 -0600

    shell-scripts

---
+++ 17-disk-usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DSIK_THRESHOLD=1
+message=""
+
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partion=$(echo $line  | awk '{print $1F}')
+    if [ $usage -gt $DSIK_THRESHOLD ]
+    then
+        message+="High Disk Usage 0n $partition: $usage"
+    fi
+done <<< $DISK_USAGE
